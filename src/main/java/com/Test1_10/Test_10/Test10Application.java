58cb9b3922689db08689c46c7e69e5d37e5cf96f
github-admin12345 <156122442+github-admin12345@users.noreply.github.com>
2025-03-16 09:27:42 +0530

    Update Test10Application.java

--- src/main/java/com/Test1_10/Test_10/Test10Application.java
+++ src/main/java/com/Test1_10/Test_10/Test10Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test10Application {
 
 	public static void main(String[] args) {
+		System.out.println("Raju");
 		SpringApplication.run(Test10Application.class, args);
 	}
 
