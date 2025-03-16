dcb7a783291a4599b004869c48c5638682e66dff
ram <annatram050604@gmail.com>
2025-03-16 09:18:48 +0530

    First Commit

---
+++ src/test/java/com/Test1_10/Test_10/Test10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test1_10.Test_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
